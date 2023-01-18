5ae9370caef5ef0ef8b5c06c66fed1cf214a2fcb
Souris Ash <souris_ash@mckinsey.com>
2023-01-18 12:15:09 +0530

    feat: add empty function for finding game score

---
+++ src/gameScoreUtils.js
@@ -0,0 +1,9 @@
+/**
+ * Calculates the score of one game. Expects an array of positive integers.
+ * @param {Array<Number>} rolls An array of positive integers that specify the rolls.
+ */
+const getScoreOfGame = (rolls) => {
+
+};
+
+module.exports = { getScoreOfGame };
