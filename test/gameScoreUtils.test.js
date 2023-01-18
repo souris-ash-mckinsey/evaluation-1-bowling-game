c5daae08b1fbaff1dbe672d3b5a0486866ce924b
Souris Ash <souris_ash@mckinsey.com>
2023-01-18 13:03:25 +0530

    chore: add test for best score of game

--- test/gameScoreUtils.test.js
+++ test/gameScoreUtils.test.js
@@ -1,4 +1,4 @@
-const { getScoreOfGame } = require('../src/gameScoreUtils');
+const { getScoreOfGame, findBestGameScore } = require('../src/gameScoreUtils');
 
 describe('Finding the score of a game given the rolls', () => {
   it('throws TypeError if a non-array is provided as argument', () => {
@@ -17,5 +17,19 @@ describe('Finding the score of a game given the rolls', () => {
     it('should pass valid test case 3', () => {
       expect(getScoreOfGame([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual(13);
     });
+  });
+});
+
+describe('Finding the best score given rolls of a game', () => {
+  it('throws TypeError if a non-array is provided as argument', () => {
+    expect(() => findBestGameScore(3)).toThrow(TypeError);
+  });
+
+  it('should give max score of given games', () => {
+    expect(findBestGameScore([
+      [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10],
+      [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ])).toEqual(90);
   });
 });
