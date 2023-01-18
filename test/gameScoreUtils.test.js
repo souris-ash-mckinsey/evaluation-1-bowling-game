be98a3219eadb5010ff57978bc1b777ab9309485
Souris Ash <souris_ash@mckinsey.com>
2023-01-18 12:43:41 +0530

    fix: test cases should not expect array

--- test/gameScoreUtils.test.js
+++ test/gameScoreUtils.test.js
@@ -7,15 +7,11 @@ describe('Finding the score of a game given the rolls', () => {
 
   describe('returns correct score when rolls are given', () => {
     it('should pass valid test case 1', () => {
-      expect(getScoreOfGame([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toEqual([90]);
+      expect(getScoreOfGame([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toEqual(90);
     });
 
     it('should pass valid test case 2', () => {
-      expect(getScoreOfGame([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toEqual([30]);
-    });
-
-    it('should pass valid test case 3', () => {
-      expect(getScoreOfGame([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual([16]);
+      expect(getScoreOfGame([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toEqual(30);
     });
   });
 });
